2086f853f4ca3c366a9736f5b75765f0bfbf04d9
will0wis <42709440+will0wis@users.noreply.github.com>
2019-06-13 20:46:38 -0400

    Trabalho 3 finalizado

--- doc/create_docs.sh
+++ doc/create_docs.sh
@@ -1,1 +1,1 @@
-javadoc -classpath ../target:../lib/ECLA.jar:../lib/DTNConsoleConnection.jar -sourcepath ../src/ -subpackages core:ui:gui:input:movement:report:routing:applications:interfaces:util
+javadoc -sourcepath ../ -subpackages core:ui:gui:input:movement:report:routing:applications:interfaces
